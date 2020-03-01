ea58b00dee9399ffcc09c54397672476a01db461
Daniël Ducro <daniel@pionect.nl>
2020-03-01 22:17:18 +0100

    version assets with npm without backend

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,13 +1,19 @@
 let mix = require('laravel-mix');
 const glob = require('glob-all');
 const tailwindcss = require('tailwindcss');
+const md5File = require('md5-file/promise');
+const replace = require('replace-in-file');
 
 if (process.env.theme) {
     mix.sass('assets/sass/theme.scss', 'public/css/', {
         sassOptions: {
             includePaths: ['./node_modules']
         }
-    }).setPublicPath('public');
+    })
+        .setPublicPath('public')
+        .then(() => {
+            versionFile('public/css/theme.css');
+        })
 } else {
     mix.js('assets/js/app.js', 'public/js/')
         .sass('assets/sass/app.scss', 'public/css/')
@@ -31,5 +37,21 @@ if (process.env.theme) {
                 test: /\.svg$/,
                 use: [{loader: 'html-loader'}]
             })
+        })
+        .then(() => {
+            versionFile('public/js/app.js');
+            versionFile('public/css/app.css');
         });
+}
+
+function versionFile(path) {
+    const file = path.split('/').pop();
+    const pattern = new RegExp(file.replace('.', '\\.') + '\\?v=[a-f0-9]{32}', 'g');
+    md5File(path).then(hash => {
+        replace({
+            files: 'public/index.html',
+            from: pattern,
+            to: file + '?v=' + hash,
+        });
+    });
 }
