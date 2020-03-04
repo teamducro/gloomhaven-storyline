b0235f706056270f5f62b6c3613158bf408919f1
Daniël Ducro <daniel@pionect.nl>
2020-03-05 00:54:37 +0100

    add tailwind important variant

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+const plugin = require('tailwindcss/plugin');
+
 module.exports = {
     theme: {
         extend: {
@@ -24,6 +26,106 @@ module.exports = {
             }
         }
     },
-    variants: {},
-    plugins: []
-}
+    variants: {
+        accessibility: ['important', 'responsive', 'focus'],
+        alignContent: ['important', 'responsive'],
+        alignItems: ['important', 'responsive'],
+        alignSelf: ['important', 'responsive'],
+        appearance: ['important', 'responsive'],
+        backgroundAttachment: ['important', 'responsive'],
+        backgroundColor: ['important', 'responsive', 'hover', 'focus'],
+        backgroundPosition: ['important', 'responsive'],
+        backgroundRepeat: ['important', 'responsive'],
+        backgroundSize: ['important', 'responsive'],
+        borderCollapse: ['important', 'responsive'],
+        borderColor: ['important', 'responsive', 'hover', 'focus'],
+        borderRadius: ['important', 'responsive'],
+        borderStyle: ['important', 'responsive'],
+        borderWidth: ['important', 'responsive'],
+        boxShadow: ['important', 'responsive', 'hover', 'focus'],
+        boxSizing: ['important', 'responsive'],
+        cursor: ['important', 'responsive'],
+        display: ['important', 'responsive'],
+        fill: ['important', 'responsive'],
+        flex: ['important', 'responsive'],
+        flexDirection: ['important', 'responsive'],
+        flexGrow: ['important', 'responsive'],
+        flexShrink: ['important', 'responsive'],
+        flexWrap: ['important', 'responsive'],
+        float: ['important', 'responsive'],
+        clear: ['important', 'responsive'],
+        fontFamily: ['important', 'responsive'],
+        fontSize: ['important', 'responsive'],
+        fontSmoothing: ['important', 'responsive'],
+        fontStyle: ['important', 'responsive'],
+        fontWeight: ['important', 'responsive', 'hover', 'focus'],
+        height: ['important', 'responsive'],
+        inset: ['important', 'responsive'],
+        justifyContent: ['important', 'responsive'],
+        letterSpacing: ['important', 'responsive'],
+        lineHeight: ['important', 'responsive'],
+        listStylePosition: ['important', 'responsive'],
+        listStyleType: ['important', 'responsive'],
+        margin: ['important', 'responsive'],
+        maxHeight: ['important', 'responsive'],
+        maxWidth: ['important', 'responsive'],
+        minHeight: ['important', 'responsive'],
+        minWidth: ['important', 'responsive'],
+        objectFit: ['important', 'responsive'],
+        objectPosition: ['important', 'responsive'],
+        opacity: ['important', 'responsive', 'hover', 'focus'],
+        order: ['important', 'responsive'],
+        outline: ['important', 'responsive', 'focus'],
+        overflow: ['important', 'responsive'],
+        padding: ['important', 'responsive'],
+        placeholderColor: ['important', 'responsive', 'focus'],
+        pointerEvents: ['important', 'responsive'],
+        position: ['important', 'responsive'],
+        resize: ['important', 'responsive'],
+        stroke: ['important', 'responsive'],
+        strokeWidth: ['important', 'responsive'],
+        tableLayout: ['important', 'responsive'],
+        textAlign: ['important', 'responsive'],
+        textColor: ['important', 'responsive', 'hover', 'focus'],
+        textDecoration: ['important', 'responsive', 'hover', 'focus'],
+        textTransform: ['important', 'responsive'],
+        userSelect: ['important', 'responsive'],
+        verticalAlign: ['important', 'responsive'],
+        visibility: ['important', 'responsive'],
+        whitespace: ['important', 'responsive'],
+        width: ['important', 'responsive'],
+        wordBreak: ['important', 'responsive'],
+        zIndex: ['important', 'responsive'],
+        gap: ['important', 'responsive'],
+        gridAutoFlow: ['important', 'responsive'],
+        gridTemplateColumns: ['important', 'responsive'],
+        gridColumn: ['important', 'responsive'],
+        gridColumnStart: ['important', 'responsive'],
+        gridColumnEnd: ['important', 'responsive'],
+        gridTemplateRows: ['important', 'responsive'],
+        gridRow: ['important', 'responsive'],
+        gridRowStart: ['important', 'responsive'],
+        gridRowEnd: ['important', 'responsive'],
+        transform: ['important', 'responsive'],
+        transformOrigin: ['important', 'responsive'],
+        scale: ['important', 'responsive', 'hover', 'focus'],
+        rotate: ['important', 'responsive', 'hover', 'focus'],
+        translate: ['important', 'responsive', 'hover', 'focus'],
+        skew: ['important', 'responsive', 'hover', 'focus'],
+        transitionProperty: ['important', 'responsive'],
+        transitionTimingFunction: ['important', 'responsive'],
+        transitionDuration: ['important', 'responsive'],
+    },
+    plugins: [
+        plugin(function ({addVariant}) {
+            addVariant('important', ({container}) => {
+                container.walkRules(rule => {
+                    rule.selector = `.\\!${rule.selector.slice(1)}`;
+                    rule.walkDecls(decl => {
+                        decl.important = true;
+                    })
+                })
+            })
+        })
+    ]
+};
