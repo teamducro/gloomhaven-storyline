f48a4b5cb73d09dac4e7668c8fde82914206a084
Daniël Ducro <daniel@pionect.nl>
2020-03-06 11:06:14 +0100

    remove npm run theme
    purgeCss whitelistPatterns /^mdc-/

--- webpack.mix.js
+++ webpack.mix.js
@@ -5,46 +5,42 @@ const tailwindcss = require('tailwindcss');
 const md5File = require('md5-file/promise');
 const replace = require('replace-in-file');
 
-if (process.env.theme) {
-    mix.sass('resources/sass/theme.scss', 'public/css/', {
+
+mix.js('resources/js/app.js', 'public/js/')
+    .sass('resources/sass/app.scss', 'public/css/')
+    .sass('resources/sass/theme.scss', 'public/css/', {
         sassOptions: {
             includePaths: ['./node_modules']
         }
     })
-        .setPublicPath('public')
-        .then(() => {
-            versionFile('public/css/theme.css');
-        })
-} else {
-    mix.js('resources/js/app.js', 'public/js/')
-        .sass('resources/sass/app.scss', 'public/css/')
-        .purgeCss({
-            content: [
-                rootPath('public/index.html'),
-                rootPath('resources/**/*.js'),
-                rootPath('resources/**/*.vue')
-            ]
-        })
-        .copy('resources/img', 'public/img')
-        .copy('resources/fonts', 'public/fonts')
-        .options({
-            processCssUrls: false,
-            postCss: [tailwindcss('./tailwind.config.js')]
-        })
-        .setPublicPath('public')
-        .override(config => {
-            config.module.rules.find(rule => rule.test.test('.svg')).exclude = /\.svg$/;
+    .purgeCss({
+        content: [
+            rootPath('public/index.html'),
+            rootPath('resources/**/*.js'),
+            rootPath('resources/**/*.vue')
+        ],
+        whitelistPatterns: [/^mdc-/, /-active$/, /-enter$/, /-leave-to$/]
+    })
+    .copy('resources/img', 'public/img')
+    .copy('resources/fonts', 'public/fonts')
+    .options({
+        processCssUrls: false,
+        postCss: [tailwindcss('./tailwind.config.js')]
+    })
+    .setPublicPath('public')
+    .override(config => {
+        config.module.rules.find(rule => rule.test.test('.svg')).exclude = /\.svg$/;
 
-            config.module.rules.push({
-                test: /\.svg$/,
-                use: [{loader: 'html-loader'}]
-            })
+        config.module.rules.push({
+            test: /\.svg$/,
+            use: [{loader: 'html-loader'}]
         })
-        .then(() => {
-            versionFile('public/js/app.js');
-            versionFile('public/css/app.css');
-        });
-}
+    })
+    .then(() => {
+        versionFile('public/js/app.js');
+        versionFile('public/css/app.css');
+        versionFile('public/css/theme.css');
+    });
 
 function versionFile(path) {
     const file = path.split('/').pop();
