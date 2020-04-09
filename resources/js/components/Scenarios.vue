b30f40cf03635a8c2d1c25052aadfd497212a548
Daniël Ducro <daniel@pionect.nl>
2020-04-09 23:33:11 +0200

    scenario list colors

--- resources/js/components/Scenarios.vue
+++ resources/js/components/Scenarios.vue
@@ -40,13 +40,15 @@
                     <span class="mdc-list-item__text flex items-center">
                         <webp :src="'/img/scenarios/' + scenario.id + '.png'"
                               class="w-16 mr-4 my-1"
-                              :alt="scenario.name"></webp>
-                        <i v-if="scenario.isRequired() || scenario.isBlocked()"
+                              :alt="scenario.name"/>
+                        <i v-if="scenario.isRequired()"
+                           class="material-icons text-required text-2xl mr-2">error_outline</i>
+                        <i v-else-if="scenario.isBlocked()"
                            class="material-icons text-incomplete text-2xl mr-2">highlight_off</i>
                         <i v-else-if="scenario.isComplete()"
-                           class="material-icons text-complete text-2xl mr-2">check_circle_outline</i>
+                           class="material-icons text-2xl mr-2">check_circle_outline</i>
                         <i v-else
-                           class="material-icons text-2xl mr-2">radio_button_unchecked</i>
+                           class="material-icons text-complete text-2xl mr-2">radio_button_unchecked</i>
                         {{ scenario.name }}
                     </span>
                 </template>
