abb0db6c4eee8f52336913065ae1d80751686777
SteveRodrigue <steve.rodrigue@gmail.com>
2020-04-20 22:16:59 -0400

    Adding quest 4 in French.

--- resources/js/lang/fr.js
+++ resources/js/lang/fr.js
@@ -5,5 +5,14 @@ export default {
         blocked: 'Bloqué',
         required: 'Obligatoire'
     },
-    quest: {}
+    quest: {
+        4: {
+            "name": "Contes de la Crypte",
+            "stages": {
+                0: "En enquêtant sur l'Ombre, vous avez découvert que les bandits de Tertre Noir sont associés à ce qui semble être un culte. Votre arrivée a perturbé une sorte de rituel. Parmi les manuscrits trouvés dans la chambre rituelle se trouvent des notes sur quelques autres lieux de pouvoir dans la région qui semblent voir une utilisation intensive avec le culte. Il semble que vous ayez l'occasion de perturber davantage les activités du culte.",
+                1: "En enquêtant sur l'Ombre, vous avez découvert que les bandits de Tertre Noir sont associés à ce qui semble être un culte. Votre arrivée a perturbé une sorte de rituel. Parmi les manuscrits trouvés dans la chambre rituelle se trouvent des notes sur quelques autres lieux de pouvoir dans la région qui semblent voir une utilisation intensive avec le culte. Il semble que vous ayez l'occasion de perturber davantage les activités du culte.<br><br>Vous avez suivi les écrits dans une ancienne crypte censée être la base des opérations du culte. Un groupe de cultistes effectuait des incantations rituelles devant un trou noir et béant dans la réalité. Avec les sectateurs et leurs sbires morts, cette faille sombre est maintenant en sommeil."
+            },
+        },
+
+    }
 }
