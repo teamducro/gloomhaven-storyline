3a58dd1b6e360d2090573793015a7d14964f03a0
SteveRodrigue <steve.rodrigue@gmail.com>
2020-04-21 07:41:48 -0400

    Removed a merge leftover and replaced a tab for spaces.

--- resources/js/lang/fr.js
+++ resources/js/lang/fr.js
@@ -29,6 +29,5 @@ exportdefault{
                 1: "En enquêtant sur l'Ombre, vous avez découvert que les bandits de Tertre Noir sont associés à ce qui semble être un culte. Votre arrivée a perturbé une sorte de rituel. Parmi les manuscrits trouvés dans la chambre rituelle se trouvent des notes sur quelques autres lieux de pouvoir dans la région qui semblent voir une utilisation intensive avec le culte. Il semble que vous ayez l'occasion de perturber davantage les activités du culte.<br><br>Vous avez suivi les écrits dans une ancienne crypte censée être la base des opérations du culte. Un groupe de cultistes effectuait des incantations rituelles devant un trou noir et béant dans la réalité. Avec les sectateurs et leurs sbires morts, cette faille sombre est maintenant en sommeil."
             },
         },
-	}
+    }
 }
->>>>>>> 23043f7840d658876e5921f4a967d56c1c2ac94e
