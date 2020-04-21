23043f7840d658876e5921f4a967d56c1c2ac94e
aelentyr <aelentyr@gmail.com>
2020-04-21 08:40:47 +0200

    Translated scenarios 1 and 2

--- resources/js/lang/fr.js
+++ resources/js/lang/fr.js
@@ -1,9 +1,26 @@
-export default {
-    general: {
-        complete: 'Achevé',
-        incomplete: 'Incomplet',
-        blocked: 'Bloqué',
-        required: 'Obligatoire'
-    },
-    quest: {}
+exportdefault{
+	general: {
+		complete: 'Achevé',
+		incomplete: 'Incomplet',
+		blocked: 'Bloqué',
+		required: 'Obligatoire'
+	},
+	quest: {
+		1: {
+			"name": "La Requête de la Négociante",
+			"stages": {
+				0: "La Négociante Valrath Jekserah a proposé de vous payer dix pièces d'or pour récupérer des biens volés. Après une conversation musclée avec certains voyous locaux, vous avez découvert que la cachette des voleurs est le Tertre Noir dans le bois du Cadavre.",
+				1: "La Négociante Valrath Jekserah a proposé de vous payer dix pièces d'or pour récupérer des biens volés. Après une conversation musclée avec des voyous locaux, vous avez découvert que la cachette des voleurs est le Tertre Noir dans le bois de cadavre. <br> <br> Vous avez trouvé votre cible dans le Terte, mais elle s'est échapée. En le poursuivant, vous avez compris pourquoi les bandits ont choisi cette sépulture particulière: des Squelettes Réanimés. Les bandits semblent avoir des dons pour la nécromantie. Vous les avez néanmoins tués, ainsi que leurs alliés impies§. Votre cible ne fait pas partie des cadavres, mais un passage plus profond dans les catacombes vous attend.",
+				2: "La Négociante Valrath Jekserah a proposé de vous payer dix pièces d'or pour récupérer des biens volés. Après une conversation musclée avec  des voyous locaux, vous avez découvert que la cachette des voleurs est le Tertre Noir. <br> <br> Vous avez trouvé votre cible dans le Terte, mais il s'est éloigné. En le poursuivant, vous avez compris pourquoi les bandits ont choisi cette sépulture particulière: des Squelettes Réanimés. Les bandits semblent avoir des dons pour la nécromantie. Vous les avez néanmoins tués, ainsi que leurs alliés impies. Votre cible ne faisait pas partie des cadavres, mais un passage plus profond dans les catacombes vous a mené à lui. <br> <br> Vous avez vaincu le Chef des bandits. Quelque chose dans son regard ne semblait pas naturel et il a mentionné «l'Ombre» pendant votre bataille. Vous avez trouvé les parchemins volés, cachés dans la chambre du Chef et les avez rendus à Jekserah en échange de la récompense convenue."
+			}
+		},
+		2: {
+			"name": "La Colère de la Négociante",
+			"stages": {
+				0: "Une tribu d'Inox de la forêt des Dagues a pillé des caravanes de Jekserah près de la capitale. Si vous pouviez en faire un exemple, elle vous paiera généreusement.",
+				1: "Une tribu d'Inox de la forêt des Dagues a pillé des caravanes de Jekserah près de la capitale. Si vous pouviez en faire un exemple, elle vous paiera généreusement. <br> <br> La tribu des Inox est décimée. Vous en avez fait un exemple brutal et personne ne menacera les caravanes de Jekserah pendant un bon moment. Vous ne vous sentez pas vraiment bien d'avoir tué des parents devant leurs enfants, mais Jekserah vous a tout de même payé.",
+				2: "Une tribu d'Inox de la forêt des Dagues a pillé des caravanes de Jekserah près de la capitale. Si vous pouviez en faire un exemple, elle vous paiera généreusement. <br> <br> Jekserah a fui Gloomhaven et a juré de se venger pour ce que vous lui avez fait. Elle ne voudra probablement plus vous payer pour faire ses missions."
+			}
+		}
+	}
 }
