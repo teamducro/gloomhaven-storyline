deb66baeff37ba84daa89c3c036640e7115a0052
Daniël Ducro <daniel@pionect.nl>
2020-05-12 23:51:23 +0200

    add noreferrer

--- resources/js/components/modals/Credits.vue
+++ resources/js/components/modals/Credits.vue
@@ -5,28 +5,36 @@
             <ul class="list-disc ml-4">
                 <li class="py-1">
                     <span class="mr-1 block sm:inline">Chart layout by
-                        <a class="link" href="https://boardgamegeek.com/user/grimtoothy" target="_blank">Grimtoothy</a>:
+                        <a class="link" target="_blank" rel="noreferrer"
+                           href="https://boardgamegeek.com/user/grimtoothy">
+                            Grimtoothy
+                        </a>:
                     </span>
-                    <a class="link" target="_blank"
+                    <a class="link" target="_blank" rel="noreferrer"
                        href="https://boardgamegeek.com/filepage/177419/scenario-and-storyline-flowchart">
                         Scenario and Storyline Flowchart
                     </a>
                 </li>
                 <li class="py-1">
                     <span class="mr-1 block sm:inline">Scenario summaries by
-                        <a class="link" href="https://boardgamegeek.com/user/DoctorBandage" target="_blank">DoctorBandage</a>:
+                        <a class="link" target="_blank" rel="noreferrer"
+                           href="https://boardgamegeek.com/user/DoctorBandage">
+                            DoctorBandage
+                        </a>:
                     </span>
-                    <a class="link" target="_blank"
+                    <a class="link" target="_blank" rel="noreferrer"
                        href="https://boardgamegeek.com/geeklist/237840/gloomhaven-quest-log">
                         Gloomhaven Quest Log
                     </a>
                 </li>
                 <li class="py-1">
                     <span class="mr-1 block sm:inline">Map coordinates by
-                        <a class="link" href="https://boardgamegeek.com/user/Dargalin"
-                           target="_blank">Dargalin</a>:
+                        <a class="link" target="_blank" rel="noreferrer"
+                           href="https://boardgamegeek.com/user/Dargalin">
+                            Dargalin
+                        </a>:
                     </span>
-                    <a class="link" target="_blank"
+                    <a class="link" target="_blank" rel="noreferrer"
                        href="https://boardgamegeek.com/thread/1880969/online-tracker-campaign-party-and-characters">
                         Online map & campaign tracker
                     </a>
