f8ed225dc20fedfdcf27c04f2f61e3d887d93230
Daniël Ducro <daniel@pionect.nl>
2020-09-05 01:17:14 +0200

    It shares the party sheet test

--- cypress/integration/share.spec.js
+++ cypress/integration/share.spec.js
@@ -11,4 +11,16 @@ describe('Share', () => {
         utilities.isNodeIncomplete(4);
     });
 
+    it('It shares the party sheet', () => {
+        cy.visit('#/shared/1/local/N4RozgjARAXKDGALAdgVwDboDQmVKOADvGpjmAC4gCWOFA2gLoC+OAhgOYDCASrKGzD0oAcV4BZKC3ZiekuCDYB3EBBzwA9mpDoQABlbhECRG3qqc2q5Zv6ce+47vOHLp672McAEwh7-ONTewFAA7NAwdmEATLBRoQDMcQ5hACzJWGEArBlhAGy5oaGFAByFAJy5JXpVEVElsZEpJUlNmSXpbVAlOV0lBX3FfWV9lV3lNeN1KeWNUeWt853zvfqGeABiGhoABABCbABOBCCE1HlZOIfRrqjmHu6PLizMQA');
+        cy.visit('/#/party');
+
+        cy.get('input[aria-labelledby="reputation"]').should('have.value', '20');
+        cy.get('input[aria-labelledby="donations"]').should('have.value', '1000');
+        cy.get('input#p-35').should('be.checked');
+        cy.get('input#p-65').should('be.checked');
+        cy.get('span').contains('064–070').prev().find('input').should('be.checked');
+        cy.get('#notes').should('have.value', 'Foo Bar');
+    });
+
 });
