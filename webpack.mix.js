12738a0130404bc7eace5ec9ff2e50aaf2094d59
Daniël Ducro <daniel@pionect.nl>
2020-10-29 00:10:04 +0100

    fixed compile css

--- webpack.mix.js
+++ webpack.mix.js
@@ -19,11 +19,11 @@ mix.extend('i18n', new class {
 
 
 mix.i18n()
-    .js('resources/js/website.js', 'public/js/')
     .js('resources/js/app.js', 'public/js/')
     .js('resources/js/gtm.js', 'public/js/')
-    .sass('resources/sass/website.scss', 'public/css/')
+    .js('resources/js/website.js', 'public/js/')
     .sass('resources/sass/app.scss', 'public/css/')
+    .sass('resources/sass/website.scss', 'public/css/')
     .sass('resources/sass/theme.scss', 'public/css/', {
         sassOptions: {
             includePaths: ['./node_modules']
