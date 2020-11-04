1e7e9a9a80065908672b2fa57d2ea33fe1b697f7
Daniël Ducro <daniel@pionect.nl>
2020-11-04 20:51:57 +0100

    color hero buttons

--- resources/js/pages/sections/HeroHeader.vue
+++ resources/js/pages/sections/HeroHeader.vue
@@ -3,7 +3,7 @@
         <div class="pt-20 lg:pt-24">
             <div class="container px-3 mx-auto flex flex-wrap flex-col md:flex-row items-center">
                 <!--Left Col-->
-                <div class="flex flex-col w-full md:w-1/2 justify-center items-start text-center md:text-left">
+                <div class="flex flex-col w-full md:w-1/2 md:pr-2 justify-center items-start text-center md:text-left">
                     <p class="uppercase tracking-loose w-full">Find the right scenario to play</p>
                     <h1 class="my-4 text-5xl font-bold leading-tight">Gloomhaven Storyline Tracker!</h1>
                     <p class="leading-normal text-2xl mb-8">Keep track of your progress of completed and unlocked
@@ -12,18 +12,18 @@
                     <div
                         class="flex flex-col w-full lg:flex-row items-center md:items-start justify-center lg:justify-start">
                         <purchase class="mb-8 lg:mb-0 lg:mr-8 cursor-pointer">
-                            <a class="cursor-pointer block text-center rounded-lg border border-transparent bg-white px-6 py-3 text-lg leading-6 font-bold text-black hover:text-gray-700 focus:outline-none focus:shadow-outline transition ease-in-out duration-150">
+                            <a class="cursor-pointer block text-center rounded-lg border border-transparent bg-blue-600 px-6 py-3 text-lg leading-6 font-bold text-white hover:bg-blue-500 focus:outline-none focus:border-blue-700 focus:shadow-outline transition ease-in-out duration-150">
                                 Buy shared campaign
                             </a>
                         </purchase>
                         <a :href="this.appUrl"
-                           class="block text-center rounded-lg border border-transparent bg-white px-6 py-3 text-lg leading-6 font-bold text-black hover:text-gray-700 focus:outline-none focus:shadow-outline transition ease-in-out duration-150">
+                           class="block text-center rounded-lg border border-transparent bg-white px-6 py-3 text-lg leading-6 font-bold text-blue-600 hover:text-blue-500 focus:outline-none focus:shadow-outline transition ease-in-out duration-150">
                             Open storyline
                         </a>
                     </div>
                 </div>
                 <!--Right Col-->
-                <div class="w-full md:w-1/2 py-6 text-center">
+                <div class="w-full md:w-1/2 py-6 text-center md:pl-2">
                     <webp class="w-full z-50" src="/img/gloomhaven-cover.png" alt="Gloomhaven starter characters"/>
                 </div>
             </div>
