06a3dcf5f4609c444063c49c4b8581acb5a1e5b3
Daniël Ducro <daniel@pionect.nl>
2020-11-21 20:28:54 +0100

    make sure changes to manual achievements are synced to the cloud

--- resources/js/repositories/AchievementRepository.js
+++ resources/js/repositories/AchievementRepository.js
@@ -4,6 +4,7 @@ import ScenarioRepository from "./ScenarioRepository";
 import AchievementGroup from "../models/AchievementGroup";
 import {ScenarioState} from "../models/ScenarioState";
 import scenarios from "../scenarios.json";
+import StorySyncer from "../services/StorySyncer";
 
 export default class AchievementRepository {
 
@@ -47,6 +48,8 @@ export default class AchievementRepository {
                 && scenarioToUnlock.state !== ScenarioState.incomplete
                 && scenarioToUnlock.state !== ScenarioState.complete) {
                 this.scenarioRepository.changeState(scenarioToUnlock, ScenarioState.incomplete);
+            } else {
+                this.storySyncer.store();
             }
         }
     }
@@ -73,6 +76,8 @@ export default class AchievementRepository {
             const scenarioToHide = this.scenarioRepository.getSideScenarioByManualAchievement(achievement);
             if (scenarioToHide && scenarioToHide.isVisible()) {
                 this.scenarioRepository.changeState(scenarioToHide, ScenarioState.hidden);
+            } else {
+                this.storySyncer.store();
             }
         }
     }
@@ -140,6 +145,10 @@ export default class AchievementRepository {
 
     get scenarioRepository() {
         return this._scenarioRepository || (this._scenarioRepository = new ScenarioRepository());
+    }
+
+    get storySyncer() {
+        return this._storySyncer || (this._storySyncer = new StorySyncer);
     }
 
 }
