b968245ad422e7e1da4b1dfa01667b7e38ff3b57
Daniël Ducro <daniel@pionect.nl>
2021-02-02 02:22:34 +0100

    added missing icons to item desc

--- resources/js/components/modals/ItemModel.vue
+++ resources/js/components/modals/ItemModel.vue
@@ -1,11 +1,18 @@
 <template>
     <div>
-        <modal ref="modal" :title="item ? item.name : ''" max-width="450px">
+        <modal ref="modal" :title="item ? item.number : ''" max-width="450px">
             <div slot="content" class="w-full h-full outline-none">
                 <webp v-if="item"
                       :src="item.image"
                       :alt="item.name"
                       class="w-full rounded-3xl" style="max-width: 400px;"/>
+
+                <p class="mt-2" v-if="item.faq">
+                    <span class="relative w-6 h-4 inline-block">
+                        <span class="material-icons absolute">help_outline</span>
+                    </span>
+                    {{ item.faq }}
+                </p>
             </div>
         </modal>
     </div>
