868795e6b76e52f4bdb6d50d91d5cb0efe542b6c
Daniël Ducro <daniel@pionect.nl>
2021-02-07 10:22:22 +0100

    slugify toString

--- resources/js/services/Slugify.js
+++ resources/js/services/Slugify.js
@@ -3,7 +3,7 @@ import slugify from 'slugify';
 export default {
     methods: {
         slugify: function (string) {
-            return slugify(string, {lower: true});
+            return slugify(String.toString(string), {lower: true});
         }
     }
 }
