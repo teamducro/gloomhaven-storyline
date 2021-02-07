347433f917c2066d8e387936a5da94fe8037db1b
Daniël Ducro <daniel@pionect.nl>
2021-02-07 10:03:57 +0100

    add replaceAll polyfill

--- resources/js/app.js
+++ resources/js/app.js
@@ -22,6 +22,26 @@ import ItemRepository from "./repositories/ItemRepository";
 import * as Sentry from "@sentry/vue";
 import {Integrations} from "@sentry/tracing";
 
+/**
+ * String.prototype.replaceAll() polyfill
+ * https://gomakethings.com/how-to-replace-a-section-of-a-string-with-another-one-with-vanilla-js/
+ * @author Chris Ferdinandi
+ * @license MIT
+ */
+if (!String.prototype.replaceAll) {
+    String.prototype.replaceAll = function (str, newStr) {
+
+        // If a regex pattern
+        if (Object.prototype.toString.call(str).toLowerCase() === '[object regexp]') {
+            return this.replace(str, newStr);
+        }
+
+        // If a string
+        return this.replace(new RegExp(str, 'g'), newStr);
+
+    };
+}
+
 window._ = require('lodash');
 window.$ = require('jquery');
 window.Vue = require('vue');
