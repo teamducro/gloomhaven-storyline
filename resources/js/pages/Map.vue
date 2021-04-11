cdf23c735a0e4019d6fe27f2da502cc59a08236c
Daniël Ducro <daniel@pionect.nl>
2021-04-11 20:28:46 +0200

    Hide scenario 96 from map if 51 isn’t completed.

--- resources/js/pages/Map.vue
+++ resources/js/pages/Map.vue
@@ -26,7 +26,7 @@
             </ul>
             <template v-if="scenarios">
                 <webp v-for="scenario in scenarios.items"
-                      v-if="scenario.isVisible()"
+                      v-if="scenario.isVisible() && (!scenario.root || (scenario.root && !scenario.isRequired()))"
                       :src="scenario.image()"
                       :key="scenario.id"
                       :id="'s' + scenario.id"
