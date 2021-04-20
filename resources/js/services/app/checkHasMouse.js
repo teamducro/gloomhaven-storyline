1edb07abd312a0243aaf5d1b21bfc2aaf2917111
Daniël Ducro <daniel@pionect.nl>
2021-04-20 10:52:31 +0200

    Revert "checkHasMouse remove jQuery"
    This reverts commit e24729db24fb05cb88105acbe77d474ad83bc4fa.

--- resources/js/services/app/checkHasMouse.js
+++ resources/js/services/app/checkHasMouse.js
@@ -1,14 +1,8 @@
 export default function checkHasMouse($bus) {
-    const body = document.getElementsByTagName('body')[0];
-
-    const emitHasMouse = (e) => {
+    $('body').one('touchstart.test', (e) => {
+        $('body').off('mousemove.test');
+    }).one('mousemove.test', (e) => {
         $bus.$emit('has-mouse', true);
         $bus.$emit('scenarios-updated');
-    }
-
-    body.addEventListener('touchstart', () => {
-        body.removeEventListener('mousemove', emitHasMouse);
-    }, {once: true});
-
-    body.addEventListener('mousemove', emitHasMouse, {once: true});
+    });
 }
