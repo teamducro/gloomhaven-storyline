c7ad1253604423a7625330b41bdc79f0ec09edd3
jcarlosorte <jcarlosorte@ugr.es>
2021-04-29 02:04:13 +0200

    Update es.js
    update prompts

--- resources/js/lang/es.js
+++ resources/js/lang/es.js
@@ -966,13 +966,62 @@ export default {
             }
         },
     },
-    prompt: {
-        dragonChoice: {
-            title: "Amigo o Enemigo",
-            text: "You retrieved the Drake's treasure. Do you return it to the Drake or do you follow the Guard Captain's orders and attempt to kill the Drake?",
-            dragonChoice1: "Return the treasure to the Drake. Gain the Global Achivement \"The Drake Aided\". Add City Event 75 and Road Event 66 to the deck",
-            dragonChoice2: "Do not aid the Drake."
-
+    prompts: {
+        dragons: {
+            title: "Amigo o enemigo",
+            text: "You retrieved the Drake’s treasure. Do you return it to the Drake or do you follow the Guard Captain’s orders and attempt to kill the Drake?",
+            1: "Return the treasure to the Drake. Gain the Global Achievement “The Drake Aided”. Add City Event 75 and Road Event 66 to the deck",
+            2: "Do not aid the Drake."
+        },
+        burningMountain: {
+            title: "Escape The Burning Mountain",
+            text: "You looted the treasure. Do you dare to escape the rumbling volcano and keep the retrieved items?",
+            1: "Conclusion A: You decide to end your turn at G and remove one of the items from the game.",
+            2: "Conclusion B: After looting the chest, all characters returned to the entrance before becoming exhausted."
+        },
+        merchantsBay: {
+            title: "The Merchant’s Bay",
+            text: "You doused the flames and disposed the interlopers. How many records were you able to save?",
+            1: "You recovered both scrolls.",
+            2: "You failed to recover the Record of the Journey.",
+            3: "You failed to recover the Record of Accounts."
+        },
+        shiftingGears: {
+            title: "Bastian’s workshop",
+            text: "Did you manage to kill all enemies?",
+            1: "All enemies are killed.",
+            2: "???"
+        },
+        searchPiecesOfAnArtifact: {
+            title: "Search pieces of the rift device",
+            text: "Did you manage to kill all enemies?",
+            1: "All enemies are killed.",
+            2: "Found more pieces of the rift device."
+        },
+        directingBeams: {
+            title: "Directing Beams",
+            text: "Did you direct the beams to the fountain?",
+            1: "All four beams are directed at the fountain",
+            2: "Survived for fourteen rounds"
+        },
+        aftershocks: {
+            title: "Protect the civilians",
+            text: "How many Valraths were you able to bring to safety?",
+            1: "Exactly four civilians are alive.",
+            2: "Five or more civilians are alive."
+        },
+        bloodyWar: {
+            title: "Bloody Demon War",
+            text: "Which side dit you choose?",
+            1: "A: Assist the Valraths",
+            2: "B: Assist the Humans",
+            3: "C: Fight both sides"
+        },
+        escapeFromTheHusk: {
+            title: "Escape from The Husk",
+            text: "Did all characters escaped through an exit?",
+            1: "All characters escaped",
+            2: "???"
         }
     }
 }
