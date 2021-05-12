db2bf1a272a9be218ff6fa6b35244f6ae4a17720
jcarlosorte <jcarlosorte@ugr.es>
2021-05-12 11:55:07 +0200

    Update es.js
    Update "promps" section

--- resources/js/lang/es.js
+++ resources/js/lang/es.js
@@ -1266,62 +1266,62 @@ export default {
             }
         }
     },
-    prompts: {
-        dragons: {
-            title: "Amigo o enemigo",
-            text: "Recuperasteis el tesoro del Draco. ¿Se lo devolvéis al Draco o aceptásis el trabajo de intentar matar al Draco?",
-            1: "Devolvéis el tesoro al Draco. Obtienes el logro global “El Draco Auxiliado”. Agrega el Evento de Ciudad 75 y el Evento de Viaje 66 al mazo",
-            2: "No ayudásteis al Draco ."
+    "prompts": {
+        "dragons": {
+            "1": "Devolvéis el tesoro al Draco. Obtienes el logro global “El Draco Auxiliado”. Agrega el Evento de Ciudad 75 y el Evento de Viaje 66 al mazo",
+            "2": "No ayudásteis al Draco .",
+            "title": "Amigo o enemigo",
+            "text": "Recuperasteis el tesoro del Draco. ¿Se lo devolvéis al Draco o aceptásis el trabajo de intentar matar al Draco?"
         },
-        burningMountain: {
-            title: "Escapar de la montaña ardiente",
-            text: "Saqueasteis el tesoro. ¿Intentáis escapar con todos los artefactos pese a las advertencias?",
-            1: "Conclusión A: Decides terminar tu turno en 'G' y retirar uno de los objetos del juego.",
-            2: "Conclusión B: Después de saquear el tesoro, todos los personajes regresaron a la entrada antes de agotarse. "
+        "burningMountain": {
+            "1": "Conclusión A: Decides terminar tu turno en 'G' y retirar uno de los objetos del juego.",
+            "2": "Conclusión B: Después de saquear el tesoro, todos los personajes regresaron a la entrada antes de agotarse. ",
+            "title": "Escapar de la montaña ardiente",
+            "text": "Saqueasteis el tesoro. ¿Intentáis escapar con todos los artefactos pese a las advertencias?"
         },
-        merchantsBay: {
-            title: "The Merchant’s Bay",
-            text: "You doused the flames and disposed the interlopers. How many records were you able to save?",
-            1: "You recovered both scrolls.",
-            2: "You failed to recover the Record of the Journey.",
-            3: "You failed to recover the Record of Accounts."
+        "merchantsBay": {
+            "1": "You recovered both scrolls.",
+            "2": "You failed to recover the Record of the Journey.",
+            "3": "You failed to recover the Record of Accounts.",
+            "title": "The Merchant’s Bay",
+            "text": "You doused the flames and disposed the interlopers. How many records were you able to save?"
         },
-        shiftingGears: {
-            title: "Bastian’s workshop",
-            text: "Did you manage to kill all enemies?",
-            1: "All enemies are killed.",
-            2: "???"
+        "shiftingGears": {
+            "1": "All enemies are killed.",
+            "2": "???",
+            "title": "Bastian’s workshop",
+            "text": "Did you manage to kill all enemies?"
         },
-        searchPiecesOfAnArtifact: {
-            title: "Search pieces of the rift device",
-            text: "Did you manage to kill all enemies?",
-            1: "All enemies are killed.",
-            2: "Found more pieces of the rift device."
+        "searchPiecesOfAnArtifact": {
+            "1": "All enemies are killed.",
+            "2": "Found more pieces of the rift device.",
+            "title": "Search pieces of the rift device",
+            "text": "Did you manage to kill all enemies?"
         },
-        directingBeams: {
-            title: "Directing Beams",
-            text: "Did you direct the beams to the fountain?",
-            1: "All four beams are directed at the fountain",
-            2: "Survived for fourteen rounds"
+        "directingBeams": {
+            "1": "All four beams are directed at the fountain",
+            "2": "Survived for fourteen rounds",
+            "title": "Directing Beams",
+            "text": "Did you direct the beams to the fountain?"
         },
-        aftershocks: {
-            title: "Protect the civilians",
-            text: "How many Valraths were you able to bring to safety?",
-            1: "Exactly four civilians are alive.",
-            2: "Five or more civilians are alive."
+        "aftershocks": {
+            "1": "Exactly four civilians are alive.",
+            "2": "Five or more civilians are alive.",
+            "title": "Protect the civilians",
+            "text": "How many Valraths were you able to bring to safety?"
         },
-        bloodyWar: {
-            title: "Bloody Demon War",
-            text: "Which side dit you choose?",
-            1: "A: Assist the Valraths",
-            2: "B: Assist the Humans",
-            3: "C: Fight both sides"
+        "bloodyWar": {
+            "1": "A: Assist the Valraths",
+            "2": "B: Assist the Humans",
+            "3": "C: Fight both sides",
+            "title": "Bloody Demon War",
+            "text": "Which side dit you choose?"
         },
-        escapeFromTheHusk: {
-            title: "Escape from The Husk",
-            text: "Did all characters escaped through an exit?",
-            1: "All characters escaped",
-            2: "???"
+        "escapeFromTheHusk": {
+            "1": "All characters escaped",
+            "2": "???",
+            "title": "Escape from The Husk",
+            "text": "Did all characters escaped through an exit?"
         }
     }
 }
