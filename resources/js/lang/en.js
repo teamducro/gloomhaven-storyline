df396381b94ee275f857aae52a659913806a1479
Daniël Ducro <daniel@pionect.nl>
2021-07-05 01:26:58 +0200

    Revert "characters init"
    This reverts commit 1c179f63d9ae10d2d8772d130f2a87299866af6a.

--- resources/js/lang/en.js
+++ resources/js/lang/en.js
@@ -10,8 +10,6 @@ export default {
     "Open source": "Open source",
     "Info": "Info",
     "Settings": "Settings",
-    "Characters": "Characters",
-    "Character sheet": "Character sheet",
     "Party sheet": "Party sheet",
     "Reputation": "Reputation",
     "Shop modifier": "Shop modifier",
