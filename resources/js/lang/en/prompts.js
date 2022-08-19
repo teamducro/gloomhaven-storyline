a0fe9e35bdd2958a3973a2fa760357be990aedbd
Joe <jgwehr@users.noreply.github.com>
2022-08-19 10:23:55 -0500

    Crimson Scale Add-On scenarios

--- resources/js/lang/en/prompts.js
+++ resources/js/lang/en/prompts.js
@@ -61,5 +61,18 @@ export default {
         "2": "Tell Selandre the potion was destroyed.",
         "title": "Can you trust Selandre?",
         "text": "Do you listen to Dominic's last words or your greed?"
-    }
+    },
+    "capstoneTest":{
+        "1": "One named enemy is killed",
+        "2": "Two named enemies are killed",
+        "3": "Three named enemies are killed",
+        "title": "Test your Power Modulator",
+        "text": "How many named Enemies were you able to kill before exhausting?"
+    },
+    "unseenPlight": {
+        "1": "Killed the Guttermaw, claiming Guttermaw's Bounty",
+        "2": "Saved the children, fulfilling Feigrin's Request",
+        "title": "",
+        "text": ""
+    },
 }
