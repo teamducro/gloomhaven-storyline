20b93ef5f42eb58728ab964490865879503929af
Daniël <daniel@pionect.nl>
2022-12-28 02:05:08 +0100

    Don't hide toast on hover

--- resources/js/components/elements/Toast.vue
+++ resources/js/components/elements/Toast.vue
@@ -1,6 +1,11 @@
 <template>
     <transition name="fade">
-        <div v-if="show" class="toast fixed right-0 bottom-0 mr-4 mb-4">
+        <div v-if="show" class="toast fixed right-0 bottom-0 mr-4 mb-4"
+             @mouseover="hoverStart"
+             @mouseleave="hoverEnd"
+             @touchstart="hoverStart"
+             @touchend="hoverEnd"
+             @touchcancel="hoverEnd">
             <alert :type="success ? 'success' : 'error'">
                 <component v-bind:is="render()"></component>
             </alert>
@@ -17,7 +22,8 @@ export default {
             message: '',
             show: false,
             success: true,
-            timer: null
+            timer: null,
+            seconds: 5
         }
     },
     mounted() {
@@ -36,12 +42,18 @@ export default {
             this.message = message;
             this.success = isSuccess;
             this.show = true;
+            this.hoverStart();
+            this.hoverEnd();
+        },
+        hoverStart() {
             if (this.timer) {
                 clearTimeout(this.timer);
             }
+        },
+        hoverEnd() {
             this.timer = setTimeout(() => {
                 this.show = false;
-            }, 5000);
+            }, this.seconds * 1000);
         }
     }
 }
