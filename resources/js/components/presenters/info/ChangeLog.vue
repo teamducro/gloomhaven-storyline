d3a1dfd37d4118ce4bf8143aacd166fdfb43f03b
Daniël <daniel@pionect.nl>
2023-02-19 21:29:36 +0100

    3.14

--- resources/js/components/presenters/info/ChangeLog.vue
+++ resources/js/components/presenters/info/ChangeLog.vue
@@ -141,6 +141,7 @@ export default {
                 {version: '3.11', description: 'Crimson Scales out of beta and add support to use items across games', date: dayjs('2023-01-02')},
                 {version: '3.12', description: 'A read only sharing option for shared campaigns', date: dayjs('2023-01-09')},
                 {version: '3.13', description: 'Added attack modifier cards to character sheets', date: dayjs('2023-01-21')},
+                {version: '3.14', description: 'Change font', date: dayjs('2023-02-19')},
             ].reverse()
         }
     },
