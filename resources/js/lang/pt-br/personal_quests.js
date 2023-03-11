c9de4731545892b47507e23e2bfee7ad1fae17c7
Lucas Thibau <lucas.paulino@ab-inbev.com>
2023-03-11 13:23:20 -0300

    Initial commit for pt-br Scenarios for GH base finished General partially done Achievements partially done Treasures for GH base finished

---
+++ resources/js/lang/pt-br/personal_quests.js
@@ -0,0 +1,338 @@
+export default {
+    "gh-510": {
+        "name": "Seeker of Xorn",
+        "progress": [
+            "Complete three Crypt scenarios.",
+            "Conclude {SCENARIO 52}.",
+        ]
+    },
+    "gh-511": {
+        "name": "Merchant Class",
+        "progress": [
+            "Own two {BODY} items.",
+            "Own two {HEAD} items.",
+            "Own two {LEGS} items.",
+            "Own three {ONE-HAND} or {TWO-HANDS} items.",
+            "Own four {SMALL-ITEM} items.",
+        ]
+    },
+    "gh-512": {
+        "name": "Greed is Good",
+        "progress": [
+            "Have 200 gold in your possession.",
+        ]
+    },
+    "gh-513": {
+        "name": "Finding the Cure",
+        "unlock": "Abram o envelope X",
+        "progress": [
+            "Kill eight Forest Imps.",
+            "Conclude {SCENARIO 59}.",
+        ]
+    },
+    "gh-514": {
+        "name": "A Study of Anatomy",
+        "progress": [
+            "Experience your party members becoming exhausted fifteen times.",
+        ]
+    },
+    "gh-515": {
+        "name": "Law Bringer",
+        "progress": [
+            "Kill twenty Bandits or Cultists.",
+        ]
+    },
+    "gh-516": {
+        "name": "Pounds of Flesh",
+        "progress": [
+            "Kill fifteen Vermlings.",
+        ]
+    },
+    "gh-517": {
+        "name": "Trophy Hunt",
+        "progress": [
+            "Kill twenty different types of monsters.",
+        ]
+    },
+    "gh-518": {
+        "name": "Eternal Wanderer",
+        "progress": [
+            "Complete fifteen different scenarios.",
+        ]
+    },
+    "gh-519": {
+        "name": "Battle Legend",
+        "progress": [
+            "Earn fifteen ✔ from completed battle goals.",
+        ]
+    },
+    "gh-520": {
+        "name": "Implement of Light",
+        "progress": [
+            "Find the Skullbane Axe in the Necromancer's Sanctum.",
+            "Use it to kill seven Living Bones, Living Corpses, or Living Spirits.",
+        ]
+    },
+    "gh-521": {
+        "name": "Take Back the Trees",
+        "progress": [
+            "Complete three scenarios in the Dagger Forest.",
+            "Conclude {SCENARIO 55}.",
+        ]
+    },
+    "gh-522": {
+        "name": "The Thin Places",
+        "progress": [
+            "Complete six side scenarios.",
+        ]
+    },
+    "gh-523": {
+        "name": "Aberrant Slayer",
+        "progress": [
+            "Kill one Flame Demon.",
+            "Kill one Frost Demon.",
+            "Kill one Wind Demon.",
+            "Kill one Earth Demon.",
+            "Kill one Night Demon.",
+            "Kill one Sun Demon.",
+        ]
+    },
+    "gh-524": {
+        "name": "Fearless Stand",
+        "progress": [
+            "Kill twenty elite monsters.",
+        ]
+    },
+    "gh-525": {
+        "name": "Piety in All Things",
+        "progress": [
+            "Donate 120 gold to the Sanctuary of the Great Oak.",
+        ]
+    },
+    "gh-526": {
+        "name": "Vengeance",
+        "unlock": "Open Envelope X",
+        "progress": [
+            "Complete four scenarios in Gloomhaven.",
+            "Conclude {SCENARIO 57}.",
+        ]
+    },
+    "gh-527": {
+        "name": "Zealot of the Blood God",
+        "progress": [
+            "Become exhausted twelve times.",
+        ]
+    },
+    "gh-528": {
+        "name": "Goliath Toppler",
+        "progress": [
+            "Complete four boss scenarios",
+        ]
+    },
+    "gh-529": {
+        "name": "The Fall of Man",
+        "progress": [
+            "Complete 2 Lingering Swamp scenarios.",
+            "Conclude {SCENARIO 61}.",
+        ]
+    },
+    "gh-530": {
+        "name": "Augmented Abilities",
+        "progress": [
+            "Purchase four enhancements.",
+        ]
+    },
+    "gh-531": {
+        "name": "Elemental Samples",
+        "progress": [
+            "Complete a scenario in Gloomhaven.",
+            "Complete a scenario in Dagger Forest.",
+            "Complete a scenario in Lingering Swamp",
+            "Complete a scenario in Watcher Mountains",
+            "Complete a scenario in Copperneck Mountains",
+            "Complete a scenario in Misty Sea.",
+        ]
+    },
+    "gh-532": {
+        "name": "A Helping Hand",
+        "progress": [
+            "Experience two other characters achieving their personal quests.",
+        ]
+    },
+    "gh-533": {
+        "name": "The Perfect Poison",
+        "progress": [
+            "Kill three Oozes.",
+            "Kill three Lurkers.",
+            "Kill three Spitting Drakes.",
+        ]
+    },
+    "cs-330": {
+        "name": "Protect and Serve",
+        "progress": [
+            "Goal met"
+        ]
+    },
+    "cs-331": {
+        "name": "Weapons Specialist",
+        "progress": [
+            "Goal met"
+        ]
+    },
+    "cs-332": {
+        "name": "Experimentation",
+        "progress": [
+            "Goal met"
+        ]
+    },
+    "cs-333": {
+        "name": "Thrill Seeker",
+        "progress": [
+            "Goal met"
+        ]
+    },
+    "cs-334": {
+        "name": "Trap Setter",
+        "progress": [
+            "Goal met"
+        ]
+    },
+    "cs-335": {
+        "name": "Bandit Banisher",
+        "progress": [
+            "Goal met"
+        ]
+    },
+    "cs-336": {
+        "name": "Creatures in the light",
+        "progress": [
+            "Goal met"
+        ]
+    },
+    "cs-337": {
+        "name": "Experienced leader",
+        "progress": [
+            "Goal met"
+        ]
+    },
+    "cs-338": {
+        "name": "Adrenaline Spike",
+        "progress": [
+            "Goal met"
+        ]
+    },
+    "cs-339": {
+        "name": "Mutual Support",
+        "progress": [
+            "Goal met"
+        ]
+    },
+    "cs-340": {
+        "name": "Thy be Blessed",
+        "progress": [
+            "Goal met"
+        ]
+    },
+    "cs-341": {
+        "name": "Spiritual Cains",
+        "progress": [
+            "Goal met"
+        ]
+    },
+    "cs-342": {
+        "name": "The Path of Agony",
+        "progress": [
+            "Goal met"
+        ]
+    },
+    "cs-343": {
+        "name": "The Oying of light",
+        "progress": [
+            "Goal met"
+        ]
+    },
+    "cs-344": {
+        "name": "Natural Selection",
+        "progress": [
+            "Goal met"
+        ]
+    },
+    "cs-345": {
+        "name": "Predator and Prey",
+        "progress": [
+            "Goal met"
+        ]
+    },
+    "cs-346": {
+        "name": "An Adder Divides",
+        "progress": [
+            "Goal met"
+        ]
+    },
+    "cs-347": {
+        "name": "Field Research",
+        "progress": [
+            "Goal met"
+        ]
+    },
+    "cs-348": {
+        "name": "Conjurers Hand",
+        "progress": [
+            "Goal met"
+        ]
+    },
+    "cs-349": {
+        "name": "No Rest for the Wicked",
+        "progress": [
+            "Goal met"
+        ]
+    },
+    "cs-350": {
+        "name": "Health First",
+        "progress": [
+            "Goal met"
+        ]
+    },
+    "cs-351": {
+        "name": "Limitless Searching",
+        "progress": [
+            "Goal met"
+        ]
+    },
+    "cs-aa-001": {
+        "name": "At All Costs",
+        "progress": [
+            "Goal met"
+        ]
+    },
+    "cs-aa-002": {
+        "name": "The Weight of failure",
+        "progress": [
+            "Goal met"
+        ]
+    },
+    "cs-qa-001": {
+        "name": "Ingenious Inventor",
+        "progress": [
+            "Goal met"
+        ]
+    },
+    "cs-qa-002": {
+        "name": "Scrap Diver",
+        "progress": [
+            "Goal met"
+        ]
+    },
+    "cs-rm-001": {
+        "name": "Brutal Enforcer",
+        "progress": [
+            "Goal met"
+        ]
+    },
+    "cs-rm-002": {
+        "name": "Apex Predator",
+        "progress": [
+            "Goal met"
+        ]
+    }
+}
---
+++ resources/js/lang/pt-br/treasures.js
@@ -0,0 +1,491 @@
+export default {
+    "gh-1": {
+        "name": "Tipo de Item Aleatório",
+    },
+    "gh-2": {
+        "name": "Ganhe o “Escudo Torre” (Item 032)",
+    },
+    "gh-3": {
+        "name": "Ganhe a “Armadura de Talas” (Item 044)",
+    },
+    "gh-4": {
+        "name": "Ganhe 15 moedas de ouro",
+    },
+    "gh-5": {
+        "name": "Ganhe a “Cota de Malha” (Item 023)",
+    },
+    "gh-6": {
+        "name": "Ganhe a “Corrente com Gancho” (Item 039)",
+    },
+    "gh-7": {
+        "name": "Cenário Secundário Aleatório",
+    },
+    "gh-8": {
+        "name": "Ganhe 1 ✔",
+    },
+    "gh-9": {
+        "name": "Ganhe o “Colar de Onda” (Item 111)",
+    },
+    "gh-10": {
+        "name": "Ganhe 10 de experiência",
+    },
+    "gh-11": {
+        "name": "Tipo de Item Aleatório",
+    },
+    "gh-12": {
+        "name": "Ganhe as “Botas de Magma” (Item 099)",
+    },
+    "gh-13": {
+        "name": "Sofra 5 de dano, receba {POISON} e {WOUND}",
+    },
+    "gh-14": {
+        "name": "Ganhe 10 de experiência",
+    },
+    "gh-15": {
+        "name": "Ganhe o “Pingente dos Pacots Obscuros” (Item 045)",
+    },
+    "gh-16": {
+        "name": "Ganhe 10 moedas de ouro",
+    },
+    "gh-17": {
+        "name": "Ganhe 20 moedas de ouro",
+    },
+    "gh-18": {
+        "name": "Ganhe 15 moedas de ouro",
+    },
+    "gh-19": {
+        "name": "Tipo de Item Aleatório",
+    },
+    "gh-20": {
+        "name": "Tipo de Item Aleatório",
+    },
+    "gh-21": {
+        "name": "Sofra 5 de dano",
+    },
+    "gh-22": {
+        "name": "Tipo de Item Aleatório",
+    },
+    "gh-23": {
+        "name": "Ganhe a “Armadura de Escamas de Dragão” (Item 103)",
+    },
+    "gh-24": {
+        "name": "Sofra 5 de dano",
+    },
+    "gh-25": {
+        "name": "Ganhe 25 moedas de ouro",
+    },
+    "gh-26": {
+        "name": "Ganhe 20 moedas de ouro",
+    },
+    "gh-27": {
+        "name": "Ganhe o “Orbe do Crepúsculo” (Item 122)",
+    },
+    "gh-28": {
+        "name": "Ganhe 15 moedas de ouro",
+    },
+    "gh-29": {
+        "name": "Ganhe o “Envoltório da Resistência” (Item 097)",
+    },
+    "gh-30": {
+        "name": "Ganhe 1 ✔",
+    },
+    "gh-31": {
+        "name": "Tipo de Item Aleatório",
+    },
+    "gh-32": {
+        "name": "Tipo de Item Aleatório",
+    },
+    "gh-33": {
+        "name": "Ganhe a “Rede Pesada” (Item 019)",
+    },
+    "gh-34": {
+        "name": "Ganhe o “Amuleto da Vida” (Item 024)",
+    },
+    "gh-35": {
+        "name": "Ganhe as “Botas de Escamas de Dragão” (Item 098)",
+    },
+    "gh-36": {
+        "name": "Tipo “Botas Propulsoras” (Item 096)",
+    },
+    "gh-37": {
+        "name": "Ganhe 1 ✔",
+    },
+    "gh-38": {
+        "name": "Tipo “Anel dos Crânios” (Item 123)",
+    },
+    "gh-39": {
+        "name": "Sofra 5 de dano",
+    },
+    "gh-40": {
+        "name": "Tipo de Item Aleatório",
+    },
+    "gh-41": {
+        "name": "Ganhe a “Faca Negra” (Item 053)",
+    },
+    "gh-42": {
+        "name": "Tipo de Item Aleatório",
+    },
+    "gh-43": {
+        "name": "Sofra 5 de dano, receba {POISON} e {WOUND}",
+    },
+    "gh-44": {
+        "name": "Cenário Secundário Aleatório",
+    },
+    "gh-45": {
+        "name": "Tipo de Item Aleatório",
+    },
+    "gh-46": {
+        "name": "Sofra 3 de dano, receba {POISON}",
+    },
+    "gh-47": {
+        "name": "Ganhe a “Armadura a Vapor” (Item 104)",
+    },
+    "gh-48": {
+        "name": "Ganhe 30 de ouro",
+    },
+    "gh-49": {
+        "name": "Novo cenário: {SCENARIO 17}",
+    },
+    "gh-50": {
+        "name": "Ganhe a “Segunda Pele” (Item 101)",
+    },
+    "gh-51": {
+        "name": "Cenário Secundário Aleatório",
+    },
+    "gh-52": {
+        "name": "Ganhe o “Coração do Traidor” (Item 131)",
+    },
+    "gh-53": {
+        "name": "Tipo de Item Aleatório",
+    },
+    "gh-54": {
+        "name": "Ganhe a “Bússola Condenada” (Item 124)",
+    },
+    "gh-55": {
+        "name": "Ganhe 1 ✔",
+    },
+    "gh-56": {
+        "name": "Ganhe o “Brinco da Estrela” (Item 069)",
+    },
+    "gh-57": {
+        "name": "Ganhe 15 moedas de ouro",
+    },
+    "gh-58": {
+        "name": "Ganhe o “Elmo de Escamas de Dragão” (Item 108)",
+    },
+    "gh-59": {
+        "name": "Tipo de Item Aleatório",
+    },
+    "gh-60": {
+        "name": "Ganhe o “Machado Quebra Crânios” (Item 113)",
+    },
+    "gh-61": {
+        "name": "Ganhe o “Punhal Versátil” (Item 040)",
+    },
+    "gh-62": {
+        "name": "Ganhe o “Elmo da Montanha” (Item 110) e “Martelo Montanhoso” (Item 115)",
+    },
+    "gh-63": {
+        "name": "Tipo de Item Aleatório",
+    },
+    "gh-64": {
+        "name": "Ganhe 30 moedas de ouro",
+    },
+    "gh-65": {
+        "name": "Tipo “Elmo Chifrudo” (Item 107)",
+    },
+    "gh-66": {
+        "name": "Ganhe a “Bomba Volátil” (Item 033)",
+    },
+    "gh-67": {
+        "name": "Ganhe 10 moedas de ouro",
+    },
+    "gh-68": {
+        "name": "Ganhe o “Anel Espiral” (Item 130)",
+    },
+    "gh-69": {
+        "name": "Ganhe o “Robe da Invocação” (Item 100)",
+    },
+    "gh-70": {
+        "name": "Cenário Secundário Aleatório",
+    },
+    "gh-71": {
+        "name": "Cenário Secundário Aleatório",
+    },
+    "gh-72": {
+        "name": "Tipo “Cimitarra Flamejante” (Item 116)",
+    },
+    "gh-73": {
+        "name": "Tipo de Item Aleatório",
+    },
+    "gh-74": {
+        "name": "Tipo de Item Aleatório",
+    },
+    "gh-75": {
+        "name": "Criptograma encontrado (Veja a página 121 do livro de cenários)",
+    },
+
+    //Gloomhaven base finish here
+
+    "fc-76": {
+        "name": "Suffer 3 damage, gain {WOUND} and 10 gold",
+    },
+    "fc-77": {
+        "name": "Ganhe “Major Cure Potion” (Item 090)",
+    },
+    "fc-78": {
+        "name": "“Ancient Bow” (Item 157)",
+    },
+    "fc-79": {
+        "name": "Ganhe “Falcon Figurine” (Item 157)",
+    },
+    "fc-80": {
+        "name": "Ganhe 1 ✓ and add Rift Event 19 ao baralho",
+    },
+    "fc-81": {
+        "name": "Ganhe “Scroll of Haste” (Item 159) and 10 gold",
+    },
+    "fc-82": {
+        "name": "“Rejuvenation Greaves” (Item 158)",
+    },
+    "fc-83": {
+        "name": "Ganhe “Major Power Potion” (Item 041)",
+    },
+    "fc-84": {
+        "name": "Ganhe “Mask of Terrors” (Item 066)",
+    },
+    "fc-85": {
+        "name": "Suffer 3 damage, gain {POISON} and 10 gold",
+    },
+    "fc-86": {
+        "name": "Unlock Diviner {DR} Solo Scenario “Forecast of the Inevitable” (see page 25 of the main book)",
+    },
+    "fc-87": {
+        "name": "Suffer 2 damage, gain CURSE and 15 gold",
+    },
+    "fc-88": {
+        "name": "Ganhe 1 ✓",
+    },
+    "fc-89": {
+        "name": "Ganhe “Second Chance Ring” (Item 070)",
+    },
+    "fc-90": {
+        "name": "10 moedas de ouro para cada personagem, Read section 75 on page 49",
+    },
+    "fc-91": {
+        "name": "Ganhe 30 gold and Party Achievement: “Opportunists” Adicionem o Evento de Cidade 84 ao baralho",
+    },
+    "fc-93": {
+        "name": "Ganhe “Scroll of Haste” (Item 159)",
+    },
+    "fc-93": {
+        "name": "“Scroll of Haste” (Item 159)",
+    },
+    "fc-94": {
+        "name": "Ganhe 1 ✓ and add Rift Event 20 ao baralho",
+    },
+    "fc-95": {
+        "name": "Ganhe 25 gold and Party Achievement: “A Strongbox” Adicionem o Evento de Cidade  85 ao baralho",
+    },
+    "fc-96": {
+        "name": "You find an old tome littered with technical drawings and design schematics of a wide array of exotic machines. There is a curious handwritten note inside the front cover: {NOTE_115}",
+    },
+    "jotl-1": {
+        "name": "Ganhe 10 experience"
+    },
+    "jotl-2": {
+        "name": "“Ring of Strength” (Item 31)"
+    },
+    "jotl-3": {
+        "name": "Ganhe 15 experience"
+    },
+    "jotl-4": {
+        "name": "Ganhe 5 gold"
+    },
+    "jotl-5": {
+        "name": "Ganhe 1 ✔"
+    },
+    "jotl-6": {
+        "name": "Ganhe 10 gold"
+    },
+    "jotl-7": {
+        "name": "Ganhe 5 gold"
+    },
+    "jotl-8": {
+        "name": "Suffer 3 damage, gain {POISON,"
+    },
+    "jotl-9": {
+        "name": "Ganhe 5 gold"
+    },
+    "jotl-10": {
+        "name": "“Fateful Compass” (Item 27)"
+    },
+    "jotl-11": {
+        "name": "“Ring of Haste” (Item 30)"
+    },
+    "jotl-12": {
+        "name": "Ganhe 5 gold"
+    },
+    "jotl-13": {
+        "name": "Ganhe 5 gold"
+    },
+    "jotl-14": {
+        "name": "Ganhe 3 money tokens"
+    },
+    "jotl-15": {
+        "name": "Ganhe 1 ✔"
+    },
+    "jotl-16": {
+        "name": "“Mana Potion” (Item 14)"
+    },
+    "jotl-X": {
+        "name": "“Ring of Restoration” (Item 32)"
+    },
+    "cs-1": {
+        "name": "“Drake's Blood” (Item 14)"
+    },
+    "cs-2": {
+        "name": "“Orb of Fortune” (Item 09)"
+    },
+    "cs-3": {
+        "name": "“Chain Mace” (Item 14)"
+    },
+    "cs-4": {
+        "name": "Ganhe 15 gold and Recover one lost card"
+    },
+    "cs-5": {
+        "name": "“Concussion Mine” (Item 31)"
+    },
+    "cs-6": {
+        "name": "Ganhe 20 gold and disarm all adjacent traps"
+    },
+    "cs-7": {
+        "name": "One random “Stone” item (Item 94-100)"
+    },
+    "cs-8": {
+        "name": "“Dizzying Tincture” (Item 58)"
+    },
+    "cs-9": {
+        "name": "“Orb of Fortune” (Item 9)"
+    },
+    "cs-10": {
+        "name": "One random “Stone” item (Item 94-100)"
+    },
+    "cs-11": {
+        "name": "“Dizzying Tincture” (Item 58)"
+    },
+    "cs-12": {
+        "name": "“Mana Medicine” (Item 60)"
+    },
+    "cs-13": {
+        "name": "“Orb of Despair” (Item 10)"
+    },
+    "cs-14": {
+        "name": "“Steel Helmet” (Item 22)"
+    },
+    "cs-15": {
+        "name": "“Shifting Compass” (Item 32)"
+    },
+    "cs-16": {
+        "name": "Perform “Heal 6, Self”, gain {POISON}"
+    },
+    "cs-17": {
+        "name": "“Concussion Mine” (Item 31)"
+    },
+    "cs-18": {
+        "name": "“Boneclad Shawl” (Item 25)"
+    },
+    "cs-19": {
+        "name": "Ganhe ✓ and generate a Wild Element"
+    },
+    "cs-20": {
+        "name": "One random “Stone” item (Item 94-100)"
+    },
+    "cs-21": {
+        "name": "“Steel Helmet” (Item 22)"
+    },
+    "cs-22": {
+        "name": "“Boots of Perpetuity” (Item 19)"
+    },
+    "cs-23": {
+        "name": "“Trophy Helmet” (Item 12)"
+    },
+    "cs-24": {
+        "name": "One random “Stone” item (Item 94-100)"
+    },
+    "cs-25": {
+        "name": "“Draining Greaves” (Item 15)"
+    },
+    "cs-26": {
+        "name": "“Iron Snare” (Item 30)"
+    },
+    "cs-27": {
+        "name": "“Woven Plate Armor” (Item 23)"
+    },
+    "cs-28": {
+        "name": "“Chain Mace” (Item 14)"
+    },
+    "cs-29": {
+        "name": "“Woven Plate Armor” (Item 23)"
+    },
+    "cs-30": {
+        "name": "“Boneclad Shawl (Item 25)"
+    },
+    "cs-31": {
+        "name": "“Drake's Blood” (Item 14)"
+    },
+    "cs-32": {
+        "name": "“Curious Pendant” (Item 20)"
+    },
+    "cs-33": {
+        "name": "25  gold, gain {POISON}"
+    },
+    "cs-34": {
+        "name": "Suffer hazardous terrain damage, gain {INVISIBLE}"
+    },
+    "cs-35": {
+        "name": "“Curseblood Blade” (Item 36)"
+    },
+    "cs-36": {
+        "name": "10 experience, gain {INVSIBLE}"
+    },
+    "cs-37": {
+        "name": "“Draining Greaves” (Item 15)"
+    },
+    "cs-38": {
+        "name": "“War Pick” (Item 13)"
+    },
+    "cs-39": {
+        "name": "“Infrared Goggles” (Item 21)"
+    },
+    "cs-40": {
+        "name": "One random “Stone” item (Item 94-100)"
+    },
+    "cs-41": {
+        "name": "“Ashsteel Gauntlets” (Item 61)"
+    },
+    "cs-42": {
+        "name": "“War Pick” (Item 13)"
+    },
+    "cs-43": {
+        "name": "10 experience and Refresh all spent items"
+    },
+    "cs-44": {
+        "name": "“Mana Medicine” (Item 60)"
+    },
+    "cs-45": {
+        "name": "One random “Stone” item (Item 94-100)"
+    },
+    "cs-46": {
+        "name": "“Vipertooth Dagger” design (Item 54)"
+    },
+    "cs-47": {
+        "name": "15 gold"
+    },
+    "cs-48": {
+        "name": "15 gold"
+    },
+    "cs-49": {
+        "name": "“Magma Waders” (Item 99)"
+    }
+}
