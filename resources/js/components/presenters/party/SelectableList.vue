teamducro/gloomhaven-storyline

commit bbaea5a4a92918eb3fd728e9ef563d42b6599f4c

Campaign sheet WIP

resources/js/components/presenters/party/SelectableList.vue
@@ -2,9 +2,9 @@
     <div class="w-full mt-8">
         <div class="mb-2 flex items-center">
             <h2>{{ title }}</h2>
-            <rollback v-show="rollbackLoaded" ref="rollback"
+            <rollback :loading="rollbackLoaded" ref="rollback"
                       :value="items"
-                      @change="rolledBack"></rollback>
+                      @change="rolledBack"/>
         </div>
         <div class="flex items-center">
             <autocomplete

resources/js/pages/PartyFh.vue
@@ -1,0 +1,202 @@
+<template>
+    <div v-if="sheet" class="pt-12 pb-4 px-2 sm:px-4 md:px-8">
+        <div id="party" class="relative bg-dark-gray2-75 p-4 rounded-lg m-auto mt-4 max-w-party">
+
+            <tabs :tabs="[$t('Campaign sheet'), $t('Characters'), $t('Items')]"
+                  :icons="['assignment', 'person', 'style']"
+                  :urls="['party', 'characters', 'items']"
+                  :active="$t('Campaign sheet')"
+            />
+            <h1 class="mt-4 text-xl">{{ campaignName }}</h1>
+
+            <resources-section
+                ref="resources"
+                :resources.sync="sheet.resources"
+                :loading="loading"
+                @change="store"/>
+
+            <div class="mt-4 flex flex-wrap">
+                <counter-section
+                    class="mb-4 mr-4"
+                    ref="inspiration"
+                    :title="$t('Inspiration')"
+                    :value.sync="sheet.inspiration"
+                    :loading="loading"
+                    @change="store"/>
+
+                <counter-section
+                    class="mb-4 mr-4"
+                    ref="total-defense"
+                    :title="$t('Total Defense')"
+                    :value.sync="sheet.totalDefense"
+                    :loading="loading"
+                    @change="store"/>
+
+                <soldiers-section
+                    ref="soldiers"
+                    :sheet="sheet"
+                    :loading="loading"
+                    @change="store"/>
+            </div>
+
+            <prosperity-section
+                ref="prosperity"
+                :sheet="sheet"
+                :loading="loading"
+                :prosperity.sync="prosperity"
+                @change="store"/>
+
+            <div class="lg:flex">
+                <selectable-list
+                    id="city-events"
+                    :title="$t('City Event Decks')"
+                    :label="$t('Add city events')"
+                    :items.sync="sheet.city"
+                    @change="store"
+                    ref="city-events"
+                >
+                    <template slot="after-field" slot-scope="{checkedItems}">
+                        <button @click="draw(checkedItems, 'C')" :disabled="appData.read_only || !checkedItems.length"
+                                class="ml-4 mdc-button origin-left transform scale-90 mdc-button--raised">
+                            <i class="material-icons mdc-button__icon">launch</i>
+                            <span class="mdc-button__label">{{ $t('Draw') }}</span>
+                        </button>
+                    </template>
+                </selectable-list>
+                <selectable-list
+                    id="road-events"
+                    :title="$t('Road Event Decks')"
+                    :label="$t('Add road events')"
+                    :items.sync="sheet.road"
+                    @change="store"
+                    ref="road-events"
+                >
+                    <template slot="after-field" slot-scope="{checkedItems}">
+                        <button @click="draw(checkedItems, 'R')" :disabled="appData.read_only || !checkedItems.length"
+                                class="ml-4 mdc-button origin-left transform scale-90 mdc-button--raised">
+                            <i class="material-icons mdc-button__icon">launch</i>
+                            <span class="mdc-button__label">{{ $t('Draw') }}</span>
+                        </button>
+                    </template>
+                </selectable-list>
+            </div>
+
+            <div class="w-full mt-8">
+                <h2 class="mb-2">{{ $t('Additional notes') }}</h2>
+                <notes :value.sync="sheet.notes" id="notes" :label="$t('Notes')"
+                       @change="store" :is-local-campaign="isLocalCampaign"
+                ></notes>
+            </div>
+
+        </div>
+    </div>
+</template>
+
+<script>
+import StorySyncer from "../services/StorySyncer";
+import GetCampaignName from "../services/GetCampaignName";
+import SheetCalculations from "../services/SheetCalculations";
+import SheetRepository from "../repositories/SheetRepository";
+import ScenarioRepository from "../repositories/ScenarioRepository";
+import ResourcesSection from "../components/presenters/party/ResourcesSection.vue";
+
+export default {
+    components: {ResourcesSection},
+    mixins: [GetCampaignName, SheetCalculations],
+    inject: ['appData'],
+    data() {
+        return {
+            sheet: null,
+            prosperity: 1,
+            prosperityItems: [
+                '001–014',
+                '015–021',
+                '022–028',
+                '029–035',
+                '036–042',
+                '043–049',
+                '050–056',
+                '057–063',
+                '064–070'
+            ],
+            campaignName: null,
+            loading: true,
+            isLocalCampaign: true,
+            renderX: 0,
+            storySyncer: new StorySyncer,
+            sheetRepository: new SheetRepository,
+            scenarioRepository: new ScenarioRepository
+        }
+    },
+    mounted() {
+        this.render();
+
+        this.$bus.$on('campaigns-changed', this.render);
+        this.$bus.$on('remove-card', this.removeCard);
+    },
+    destroyed() {
+        this.$bus.$off('campaigns-changed', this.render);
+        this.$bus.$off('remove-card', this.removeCard);
+    },
+    methods: {
+        async render() {
+            this.loading = true;
+
+            this.sheet = this.sheetRepository.make(this.appData.game);
+            this.campaignName = this.getCampaignName();
+
+            this.isLocalCampaign = app.campaignId === 'local';
+
+            await this.$nextTick();
+
+            this.$refs['resources']?.reset();
+            this.$refs['morale']?.reset();
+            this.$refs['inspiration']?.reset();
+            this.$refs['total-defense']?.reset();
+            this.$refs['soldiers']?.reset();
+            this.$refs['prosperity']?.reset();
+            // this.$refs['summer-road']?.reset();
+            // this.$refs['summer-outpost']?.reset();
+            // this.$refs['winter-road']?.reset();
+            // this.$refs['winter-outpost']?.reset();
+
+            this.loading = false;
+        },
+        store() {
+            if (this.loading) {
+                return;
+            }
+
+            this.sheet.store();
+            this.storySyncer.store();
+        },
+        draw(checkedItems, type) {
+            let id = checkedItems[Math.floor(Math.random() * checkedItems.length)];
+            if (id) {
+                const card = type + '-' + id;
+                this.$bus.$emit('open-event-card', {id: card, game: this.appData.game});
+            }
+        },
+        removeCard(card) {
+            this.sheet[card.folder][card.id] = false;
+            this.store();
+        },
+        renderHtml(html) {
+            return {
+                template: `<span>${html}</span>`
+            };
+        },
+        async xResultChanged(xResult) {
+            this.sheet.xResult = xResult;
+            this.store();
+            await this.$nextTick();
+            this.sheet.fillCharacterUnlocks();
+            await this.$nextTick();
+            this.rerenderX();
+        },
+        rerenderX() {
+            this.renderX++;
+        }
+    }
+}
+</script>

resources/js/app.js
@@ -39,6 +39,7 @@ import {Flip} from "gsap/Flip.js";
 import getEnabledGames from "./services/app/getEnabledGames";
 import getFont from "./services/app/getFont";
 import BaseUrl from "./mixins/BaseUrl";
+import {Game} from "./models/Game";
 
 const {RayPlugin} = require('vue-ray/vue2');
 
@@ -213,7 +214,7 @@ window.app = new Vue({
             let items = {}
             this.enabledGames.forEach((game) => {
                 // FC uses GH items
-                if (game !== 'fc') {
+                if (game !== Game.fc) {
                     items = {...items, ...this.itemRepository.fetch(game).items};
                 }
             })
@@ -248,7 +249,7 @@ window.app = new Vue({
 
             this.campaignData = store.get(this.campaignId) || {};
             this.campaignData = migrateVersion2Progress(this.campaignData);
-            this.game = store.get('game') || 'gh';
+            this.game = store.get('game') || Game.gh;
             this.stories = this.storyRepository.getStories();
             if (Helpers.loggedIn()) {
                 this.user = this.userRepository.getUser();
@@ -289,7 +290,7 @@ window.app = new Vue({
             this.webpSupported = isWebpSupported();
             checkHasMouse(this.$bus);
             migrateVersion1Progress();
-            this.game = store.get('game') || 'gh';
+            this.game = store.get('game') || Game.gh;
             getFont();
         }
     }

resources/js/components/elements/AddLinksAndIcons.vue
@@ -16,6 +16,10 @@ export default {
         text: {
             type: String,
             required: true
+        },
+        tag: {
+            type: String,
+            default: 'span'
         }
     },
     data() {
@@ -43,7 +47,7 @@ export default {
                         scenarios
                     }
                 },
-                template: `<span>${output}</span>`
+                template: `<${this.tag}>${output}</${this.tag}>`
             };
         },
         addItemLinks(text) {
@@ -105,6 +109,7 @@ export default {
 
                     '{MODIFIER_MINUS_ONE}': '<webp src="/img/icons/general/modifier_minus_one_white.png" width="20" class="inline"/>',
                     '{-1}': '<webp src="/img/icons/general/modifier_minus_one_white.png" width="20" class="inline"/>',
+                    '{SOLDIER}': '<inline-svg src="icons/soldier"/>',
 
                     // Elements
                     '{ANY}': '<webp src="/img/icons/elements/any.png" width="20" class="inline"/>',

resources/js/components/elements/Drawer.vue
@@ -77,7 +77,7 @@
                             <router-link to="/party" class="mdc-list-item"
                                          active-class="mdc-list-item--activated">
                                 <i class="material-icons mdc-list-item__graphic" aria-hidden="true">assignment</i>
-                                <span class="mdc-list-item__text">{{ $t('Party sheet') }}</span>
+                                <span class="mdc-list-item__text">{{ game === Game.fh ? $t('Campaign sheet') : $t('Party sheet') }}</span>
                             </router-link>
                         </li>
 
@@ -178,10 +178,16 @@ import Helpers from "../../services/Helpers";
 import AuthRepository from "../../apiRepositories/AuthRepository";
 import StoryRepository from "../../repositories/StoryRepository";
 import GameData from "../../services/GameData";
+import {Game} from "../../models/Game";
 
 const md5 = require('js-md5');
 
 export default {
+    computed: {
+        Game() {
+            return Game
+        }
+    },
     inject: ['appData'],
     data() {
         return {

resources/js/components/elements/Rollback.vue
@@ -1,8 +1,10 @@
 <template>
-    <transition name="fade">
-        <span v-if="this.stack.length > 1" @click="rollback"
-              class="material-icons cursor-pointer ml-2">replay</span>
-    </transition>
+    <span class="flex ml-2 w-5" :class="{'invisible': invisible}">
+        <transition name="fade">
+            <span v-if="this.stack.length > 1" @click="rollback"
+                  class="material-icons cursor-pointer">replay</span>
+        </transition>
+    </span>
 </template>
 
 <script>
@@ -10,16 +12,26 @@ export default {
     props: {
         value: {
             default: 0
-        }
+        },
+        loading: {
+            type: Boolean,
+            default: true
+        },
     },
     data() {
         return {
             current: null,
             stack: [],
-            rollingBack: false
+            rollingBack: false,
+            invisible: true
         }
     },
     watch: {
+        loading: function () {
+            setTimeout(() => {
+                this.invisible = this.loading;
+            }, 500);
+        },
         value: {
             handler(val) {
                 this.set(val);

resources/js/components/presenters/party/CounterSection.vue
@@ -1,0 +1,65 @@
+<template>
+    <div class="">
+        <div class="mb-2 flex items-center">
+            <slot name="title">
+                <add-links-and-icons class="flex items-center gap-2" tag="h2" :text="title"/>
+            </slot>
+            <rollback :loading="loading" ref="rollback"
+                      :value.sync="currentValue"/>
+        </div>
+        <number-field :value.sync="currentValue" :min="min" :max="max" :id="slugify(title)"/>
+    </div>
+</template>
+
+<script>
+import Slugify from "../../../services/Slugify";
+
+export default {
+    mixins: [Slugify],
+    props: {
+        title: {
+            type: String,
+            required: true
+        },
+        value: {
+            type: Number,
+            default: 0
+        },
+        loading: {
+            type: Boolean,
+            default: false
+        },
+        min: {
+            type: Number,
+            default: 0
+        },
+        max: {
+            type: Number,
+            default: 999
+        }
+    },
+    data() {
+        return {
+            currentValue: this.value
+        }
+    },
+    watch: {
+        value: function (value) {
+            this.currentValue = value;
+        },
+        currentValue: function (value, oldValue) {
+            if (value === oldValue)
+                return;
+
+            // Update the parent component's value
+            this.$emit('update:value', value);
+            this.$emit('change', value);
+        }
+    },
+    methods: {
+        reset() {
+            this.$refs['rollback']?.reset();
+        }
+    }
+}
+</script>

resources/js/components/presenters/party/DonationsSection.vue
@@ -4,8 +4,8 @@
             <slot name="title">
                 <h2>{{ $t('Sanctuary of the Great Oak') }}</h2>
             </slot>
-            <rollback v-show="!loading" ref="rollback"
-                      :value.sync="sheet.donations"></rollback>
+            <rollback :loading="loading" ref="rollback"
+                      :value.sync="sheet.donations"/>
         </div>
         <number-field :value.sync="sheet.donations" :min="min" :step="step" :id="'donations'"
                       @change="$emit('change')"></number-field>

commit d12cff59de18b86080a313c1963e553f5bcfef46

Fix rolling back events, broken in bbaea5a

resources/js/components/presenters/party/SelectableList.vue
@@ -2,7 +2,7 @@
     <div class="w-full mt-8">
         <div class="mb-2 flex items-center">
             <h2>{{ title }}</h2>
-            <rollback :loading="rollbackLoaded" ref="rollback"
+            <rollback :loading="!rollbackLoaded" ref="rollback"
                       :value="items"
                       @change="rolledBack"/>
         </div>

resources/js/pages/PartyFh.vue
@@ -238,10 +238,11 @@ export default {
             this.$refs['total-defense']?.reset();
             this.$refs['soldiers']?.reset();
             this.$refs['prosperity']?.reset();
-            // this.$refs['summer-road']?.reset();
-            // this.$refs['summer-outpost']?.reset();
-            // this.$refs['winter-road']?.reset();
-            // this.$refs['winter-outpost']?.reset();
+            this.$refs['summer-road-events']?.reset();
+            this.$refs['summer-outpost-events']?.reset();
+            this.$refs['winter-road-events']?.reset();
+            this.$refs['winter-outpost-events']?.reset();
+            this.$refs['boat-events']?.reset();
 
             this.loading = false;
         },