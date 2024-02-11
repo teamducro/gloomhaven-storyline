6d68fe9eb894025780ee78c137a9338c643baf9b
Daniel Farkas <101987746+fajack87@users.noreply.github.com>
2024-02-11 15:43:10 +0100

    Add files via upload

---
+++ resources/js/lang/hu/achievements.js
@@ -0,0 +1,369 @@
+export default {
+    "gh-PFS": {
+        "name": "Első lépések",
+    },
+    "gh-PJP": {
+        "name": "Jekserah Tervei",
+    },
+    "gh-PDB": {
+        "name": "Dark Bounty",
+    },
+    "gh-GTMF": {
+        "name": "A Kereskedő Elmenekül",
+    },
+    "gh-GTDI": {
+        "name": "A holtak inváziója",
+    },
+    "gh-PADE": {
+        "name": "A Demon's Errand",
+    },
+    "gh-GCRE": {
+        "name": "Városvezetés: Kereskedők",
+    },
+    "gh-GCRM": {
+        "name": "Városvezetés: Katonaság",
+    },
+    "gh-GEOI": {
+        "name": "Az invázió vége",
+    },
+    "gh-GTPE": {
+        "name": "A varázsgömb hatalma",
+    },
+    "gh-PSC": {
+        "name": "Stonebreaker's Censer",
+    },
+    "gh-GTRN": {
+        "name": "A Síkkapu semlegesítve",
+    },
+    "gh-GAR": {
+        "name": "Az Ereklye: Megszerezve",
+    },
+    "gh-GAR2": {
+        "name": "Az Ereklye: Megszerezve (Korábban elveszett)",
+    },
+    "gh-GAL": {
+        "name": "Az Ereklye: Elveszett",
+    },
+    "gh-GAT": {
+        "name": "Ősi technológia",
+    },
+    "gh-GAT2": {
+        "name": "Ősi technológia x2",
+    },
+    "gh-GAT3": {
+        "name": "Ősi technológia x3",
+    },
+    "gh-GAT4": {
+        "name": "Ősi technológia x4",
+    },
+    "gh-GAT5": {
+        "name": "Ősi technológia x5",
+    },
+    "gh-PTR": {
+        "name": "Through the Ruins",
+    },
+    "gh-PTVC": {
+        "name": "The Voice's Command",
+    },
+    "gh-PTDC": {
+        "name": "A gyík parancsa",
+    },
+    "gh-PFC": {
+        "name": "Following Clues",
+    },
+    "gh-PAI": {
+        "name": "An Invitation",
+    },
+    "gh-GTED": {
+        "name": "A sötétség szélén",
+    },
+    "gh-PTSV": {
+        "name": "The Scepter and the Voice",
+    },
+    "gh-GAC": {
+        "name": "Az Ereklye: Megtisztítva",
+    },
+    "gh-PTVT": {
+        "name": "The Voices Treasure",
+    },
+    "gh-PTDT": {
+        "name": "A gyík kincse",
+    },
+    "gh-GTDS": {
+        "name": "The Drake Slain",
+    },
+    "gh-GTDA": {
+        "name": "A gyík megsegítése",
+    },
+    "gh-GCRD": {
+        "name": "Városvezetés: Démonok",
+    },
+    "gh-PTTT": {
+        "name": "Through the Trench",
+    },
+    "gh-PRA": {
+        "name": "Redthorn's Aid",
+    },
+    "gh-PATD": {
+        "name": "Across the Divide",
+    },
+    "gh-GTVF": {
+        "name": "A Hang kiszabadul",
+    },
+    "gh-GTVS": {
+        "name": "A Hang elhallgat",
+    },
+    "gh-GWB": {
+        "name": "Vízalatti légzés",
+    },
+    "gh-GEOC": {
+        "name": "A romlás vége",
+    },
+    "gh-GEOC2": {
+        "name": "A romlás vége x2",
+    },
+    "gh-GEOC3": {
+        "name": "A romlás vége x3",
+    },
+    "gh-GAOO": {
+        "name": "A Rend elpusztítása",
+    },
+    "gh-GEOG": {
+        "name": "A Homály végzete",
+    },
+    "gh-PTTN": {
+        "name": "Through the Nest",
+    },
+    "gh-PAMT": {
+        "name": "A Map to Treasure",
+    },
+    "gh-PBB": {
+        "name": "Bad Business",
+    },
+    "gh-PTPS": {
+        "name": "The Poison's Source",
+    },
+    "gh-PSB": {
+        "name": "Sun-Blessed",
+    },
+    "gh-PSR": {
+        "name": "Sin-Ra",
+    },
+    "gh-PDC": {
+        "name": "Debt Collection",
+    },
+    "gh-PWS": {
+        "name": "Water Staff",
+    },
+    "gh-PFA": {
+        "name": "Fish's Aid",
+    },
+    "gh-PT": {
+        "name": "Tremors",
+    },
+    "gh-PHSE": {
+        "name": "High Sea Escort",
+    },
+    "gh-PGJ": {
+        "name": "Grave Job",
+    },
+    "gh-PNE": {
+        "name": "Northern Expedition",
+    },
+    "gh-GTTP": {
+        "name": "Through the Portal",
+    },
+    "gh-PO": {
+        "name": "Opportunists",
+    },
+    "gh-GKIP": {
+        "name": "Knowledge is Power",
+    },
+    "gh-GKIP2": {
+        "name": "Knowledge is Power x2",
+    },
+    "gh-GKIP3": {
+        "name": "Knowledge is Power x3",
+    },
+    "gh-GKIP4": {
+        "name": "Knowledge is Power x4",
+    },
+    "gh-PAD": {
+        "name": "Angels of Death",
+    },
+    "gh-PC": {
+        "name": "Custodians",
+    },
+    "gh-PHP": {
+        "name": "Hunted Prey",
+    },
+    "gh-GPOA": {
+        "name": "Pieces of an Artifact",
+    },
+    "gh-GPOA2": {
+        "name": "Pieces of an Artifact x2",
+    },
+    "gh-GPOA3": {
+        "name": "Pieces of an Artifact x3",
+    },
+    "gh-PGD": {
+        "name": "Guard Detail",
+    },
+    "gh-PDE": {
+        "name": "Dimensional Equilibrium",
+    },
+    "gh-PHH": {
+        "name": "Hunting the Hunter",
+    },
+    "gh-PXA": {
+        "name": "Xangroth's Aid",
+    },
+    "gh-PDA": {
+        "name": "Diamara's Aid",
+    },
+    "gh-PBF": {
+        "name": "Beauty in Freedom",
+    },
+    "gh-GPA": {
+        "name": "A Peril Averted",
+    },
+    "gh-GPA2": {
+        "name": "A Peril Averted x2",
+    },
+    "gh-GPA3": {
+        "name": "A Peril Averted x3",
+    },
+    "gh-GPA4": {
+        "name": "A Peril Averted x4",
+    },
+    "gh-GMS": {
+        "name": "Mechanical Splendor",
+    },
+    "gh-GST": {
+        "name": "Severed Ties",
+    },
+
+    "cs-PCSOD": {
+        "name": "Ooze Destroyed",
+    },
+    "cs-PCSFL": {
+        "name": "Fallen Lava",
+    },
+    "cs-PCSFM": {
+        "name": "Follow the Money",
+    },
+    "cs-PCSFW": {
+        "name": "Frozen Warrior",
+    },
+    "cs-PCSOA": {
+        "name": "Oak Alliance",
+    },
+    "cs-PCSIA": {
+        "name": "Inox Alliance",
+    },
+    "cs-PCSGB": {
+        "name": "Guttermaw's Bounty",
+    },
+    "cs-PCSFR": {
+        "name": "Feirgrin's Request",
+    },
+    "cs-PCSPL": {
+        "name": "A Potion Lost",
+    },
+    "cs-PCSTM": {
+        "name": "Take the Money",
+    },
+    "cs-PCSAM": {
+        "name": "Accomplished Mercenaries",
+    },
+    "fh-CFIS": {
+        "name": "Friend of the Icespeakers",
+    },
+    "fh-CFSS": {
+        "name": "Friend of the Snowspeakers",
+    },
+    "fh-CFFS": {
+        "name": "Friend of the Frostspeakers",
+    },
+    "fh-CDIS": {
+        "name": "Destroyer of the Icespeakers",
+    },
+    "fh-CDSS": {
+        "name": "Destroyer of the Snowspeakers",
+    },
+    "fh-CFFK": {
+        "name": "Friend of the Fish King",
+    },
+    "fh-CCU": {
+        "name": "Crown United",
+    },
+    "fh-CUA": {
+        "name": "Unfettered Allies",
+    },
+    "fh-CUD": {
+        "name": "Unfettered Deactivated",
+    },
+    "fh-CITF": {
+        "name": "Into the Forest",
+    },
+    "fh-CFP": {
+        "name": "Firepepper",
+    },
+    "fh-CSS": {
+        "name": "Shard Seeker",
+    },
+    "fh-CEJ": {
+        "name": "Empty Jar",
+    },
+    "fh-CBJ": {
+        "name": "Bug in a Jar",
+    },
+    "fh-CUC": {
+        "name": "Uncharged Core",
+    },
+    "fh-CCC": {
+        "name": "Charged Core",
+    },
+    "fh-CHF": {
+        "name": "Harvest Festival",
+    },
+    "fh-CBT": {
+        "name": "Brummix Tracks",
+    },
+    "fh-CBT2": {
+        "name": "Brummix Tracks x2",
+    },
+    "fh-CBT3": {
+        "name": "Brummix Tracks x3",
+    },
+    "fh-CBT4": {
+        "name": "Brummix Tracks x4",
+    },
+    "fh-CBT5": {
+        "name": "Brummix Tracks x5",
+    },
+    "fh-CCCS": {
+        "name": "Coral Crown Shard",
+    },
+    "fh-CCCS2": {
+        "name": "Coral Crown Shard x2",
+    },
+    "fh-CCCS3": {
+        "name": "Coral Crown Shard x3",
+    },
+    "fh-CCCS4": {
+        "name": "Coral Crown Shard x4",
+    },
+    "fh-CCCS5": {
+        "name": "Coral Crown Shard x5",
+    },
+    "fh-CCCS6": {
+        "name": "Coral Crown Shard x6",
+    },
+    "fh-PHOI": {
+        "name": "Heart of Ice",
+    },
+    "fh-PHOS": {
+        "name": "Heart of Snow",
+    }
+}
